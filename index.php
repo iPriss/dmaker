e124230bcd105056b54776513d07cb260efe76d9
omar.yerden <omaryer@hotmail.com>
2016-06-15 22:59:00 -0300

    INit

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+?>
+
+<html lang="es">
+	<head>
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	    <title>DMaker</title>
+
+			<link href="styles/desktop.css" rel="stylesheet">
+
+			<!-- Jquery -->
+			<script src="add-ons/jquery/1.12.4/jquery.min.js"></script>
+
+			<!-- Bootstrap -->
+			<link href="add-ons/boostrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+		    <!-- Include all compiled plugins (below), or include individual files as needed -->
+		    <script src="add-ons/boostrap/3.3.6/js/bootstrap.min.js"></script>
+
+			<!-- Fontawesome -->
+		    <link href="add-ons/fontawesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<header>
+			<div id="top" class="clearfix">
+				<div class="top-r">
+					<dl class="top-buttons">
+						<dt>
+							<span>Enviar a</span>
+							<i class="fa fa-caret-down"></i>
+						</dt>
+					</dl>
+					<dl class="top-buttons">
+						<dt>
+							<span>Chat Online</span>
+							<i class="fa fa-caret-down"></i>
+						</dt>
+					</dl>
+				</div>
+			</div>
+			<div id="top-main">
+				<div class="top-main-middle">
+					<div class="top-main-middle-inner">
+						<div class="seo-field"></div>
+						<form action="" class="top-search-form">
+							<div class="search-category"></div>
+							<div class="top-search-box">
+								<input type="text" id="top_search_box">
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</header>
+	</body>
+</html>
