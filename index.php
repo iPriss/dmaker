c14b0fb17319051b10cccb873016fc1817754127
omar.yerden <omaryer@hotmail.com>
2016-06-17 20:48:42 -0300

    111

--- index.php
+++ index.php
@@ -22,6 +22,10 @@
 
 			<!-- Fontawesome -->
 		    <link href="add-ons/fontawesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+
+			<!-- Select2 -->
+		    <link href="add-ons/select2/4.0.3/css/select2.min.css" rel="stylesheet">
+			<script src="add-ons/select2/4.0.3/js/select2.min.js"></script>
 	</head>
 
 	<body>
@@ -44,17 +48,54 @@
 			</div>
 			<div id="top-main">
 				<div class="top-main-middle">
-					<div class="top-main-middle-inner">
-						<div class="seo-field"></div>
-						<form action="" class="top-search-form">
-							<div class="search-category"></div>
+					<div class="top-main-middle-inner clearfix">
+						<div class="seo-field">
+							<h1>Online Shopping</h1>:
+							<a href="www.google.com.ar">XiaoMi Mi4S </a>,
+							<a href="www.google.com.ar">HTC </a>
+						</div>
+						<form action="" class="top-search-form pr clearfix" name="searchForm">
+							<div class="search-category clearfix">
+								<div class="search-category-scope">
+									<div class="search-category-labels">
+										<span>Todas las categor&iacute;as</span>
+										<i class="fa fa-caret-down"></i>
+									</div>
+								</div>
+							</div>
 							<div class="top-search-box">
-								<input type="text" id="top_search_box">
+								<input type="text" id="top_search_box" placeholder="Palabra clave">
+								<i id="search_button" class="fa fa-search" type="submit"></i>
 							</div>
 						</form>
+					</div>
+				</div>
+				<ul class="top-links clearfix">
+					<li><a href=""><i class="fa fa-tag"></i>Ofertas</a></li>
+					<li><a href="">Pre-Venta</a></li>
+					<li><a href="">Nuevo</a></li>
+					<li><a href="">Marcas mas vendidas</a></li>
+					<li><a href="">Explorar</a></li>
+				</ul>
+				<div class="nav-bar-outer">
+					<div class="logo">
+						<!-- 56 -->
+						<a href=""><img src="imagenes/dmaker_mmm.png" height="54"></a>
+					</div>
+					<div class="nav_cats">
+						<h2>
+							<span>shop by deparment</span>
+							<i class="fa fa-caret-down"></i>
+						</h2>
 					</div>
 				</div>
 			</div>
 		</header>
 	</body>
 </html>
+
+<script type="text/javascript">
+
+	// $('select[name="category-selector"]').select2({minimumResultsForSearch: -1});
+
+</script>
